c81b64c67bd01818c21f9cce9fd2aa365effc99b
prognita <korogyianita@hotmail.com>
2018-06-18 00:01:14 +0200

    Adding new greenfoxacademy files

--- week-11/day-01/dependency/src/main/java/com/greenfoxacademy/dependency/Controller/WebController.java
+++ week-11/day-01/dependency/src/main/java/com/greenfoxacademy/dependency/Controller/WebController.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.dependency.Controller;
 
+import com.greenfoxacademy.dependency.Service.StudentService;
 import com.greenfoxacademy.dependency.Service.UtilityService;
 import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +8,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+@Controller("/useful")
 public class WebController {
+
+
 
     private final
     UtilityService utilityService;
@@ -20,6 +24,7 @@ public class WebController {
         this.utilityService = utilityService;
 
     }
+
 
     @GetMapping("/useful")
     public String index() {
@@ -42,6 +47,43 @@ public class WebController {
             model.addAttribute("valid", utilityService.validateEmail(email));
             model.addAttribute("address", email);
             return "email";
+    }
+
+    @Autowired
+    private StudentService studentService;
+
+
+
+    @RequestMapping("/useful/gfa")
+    public String gfa(Model model) {
+        model.addAttribute("numbers",studentService.findAll().size());
+        return "gfa";
+    }
+
+    @RequestMapping("/useful/gfa/list")
+    public String gfalist(Model model) {
+        model.addAttribute("gfalist",studentService.findAll());
+        return "list";
+    }
+
+    @RequestMapping("/useful/gfa/add")
+    public String addgfa(Model model) {
+
+        return "add";
+    }
+
+
+    @RequestMapping(value = "/useful/gfa/save",  method = RequestMethod.POST)
+    public String savegfa(Model model, @RequestParam("name") String name) {
+            model.addAttribute("name", name);
+            studentService.save(name);
+        return "redirect:/list.html";
+    }
+
+    @RequestMapping("/useful/gfa/check")
+    public String checkgfa(Model model) {
+
+        return "check";
     }
 
 }
---
+++ week-11/day-01/dependency/src/main/java/com/greenfoxacademy/dependency/Service/StudentService.java
@@ -0,0 +1,39 @@
+package com.greenfoxacademy.dependency.Service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    ArrayList<String> names;
+
+    public StudentService() {
+        names = new ArrayList<>();
+        names.add("Sanyi");
+        names.add("Lilla");
+        names.add("John");
+    }
+
+    public List<String> findAll() {
+        return names;
+    }
+
+    public void save(String student) {
+        names.add(student);
+    }
+
+    public ArrayList<String> getNames() {
+        return names;
+    }
+
+    public void setNames(ArrayList<String> names) {
+        this.names = names;
+    }
+
+    public void Count(){
+
+    }
+}
